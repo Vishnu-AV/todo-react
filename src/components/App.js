6f2645e744c6bebd47ba14288e72a8f06981fec8
Vishnu-AV <vishnu.av@perfomatix.com>
2022-01-11 10:33:54 +0530

    added useState abd useCallback

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TodoList from './TodoList';
 
+const ITEMS_INITIAL_STATE = [
+  {
+    id: 1,
+    text: 'Learn Javascript',
+    completed: false
+  },
+  {
+    id: 2,
+    text: 'Learn React',
+    completed: false
+  },
+  {
+    id: 3,
+    text: 'Build a React App',
+    completed: false
+  }
+];
+
+
 function App() {
-  let items = [
-    {
-      id: 1,
-      text: 'Learn Javascript',
-      completed: false
-    },
-    {
-      id: 2,
-      text: 'Learn React',
-      completed: false
-    },
-    {
-      id: 3,
-      text: 'Build a React App',
-      completed: false
-    }
-  ];
+  
   let title = 'Things to do';
+  const [items] = useState(ITEMS_INITIAL_STATE);
 
   return (
     <div className="container">
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import { KEY_RETURN } from 'keycode-js';
 
 function InputBox() {
-  return <input type="text" className="form-control add-todo" placeholder="Add New" />;
+  const [value, setValue] = useState('');
+  const handleKeyUpEvent = useCallback(e => {
+    if (e.keyCode === KEY_RETURN) {
+      // Add new Todo Here
+      // Clear the text box
+      console.log('KEY_RETURN pressed');
+    }
+  }, []);
+  const handleChangeEvent = useCallback(
+    e => {
+      setValue(e.target.value);
+    },
+    [setValue]
+  );
+
+  return (
+    <input
+      type="text"
+      className="form-control add-todo"
+      value={value}
+      onKeyUp={handleKeyUpEvent}
+      onChange={handleChangeEvent}
+      placeholder="Add New"
+    />
+  );
 }
 
 export default InputBox;
